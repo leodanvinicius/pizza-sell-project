a5c76cb12a24b63c976ba29218d2df72a2810f17
Leodan Vinicius <leo.vini_moraes@hotmail.com>
2021-09-10 16:00:59 -0300

    Adicao das pizzas e seus atributos

---
+++ script.js
@@ -0,0 +1,13 @@
+const c = (element) => document.querySelector(element);
+const cs = (element) => document.querySelectorAll(element);
+
+pizzaJson.map(function(item, index) {
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+    c('.pizza-area').append(pizzaItem);
+});
