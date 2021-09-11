cf02ae0c595cfb8f62e153a58d3ecfbcc3525a80
Leodan Vinicius <leo.vini_moraes@hotmail.com>
2021-09-11 20:00:01 -0300

    Parte 2 modal funcional (alterar quantidade, escolher tamanho)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let modalQt = 1;
 const c = (element) => document.querySelector(element);
 const ca = (element) => document.querySelectorAll(element);
 
+//Listagem das pizzas
 pizzaJson.map(function(item, index) {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
@@ -39,4 +40,36 @@ pizzaJson.map(function(item, index) {
     });
 
     c('.pizza-area').append(pizzaItem);
+});
+
+//Eventos do modal
+let close = () => {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+};
+
+ca('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', close);
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+
+});
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+
+});
+
+ca('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
 });
